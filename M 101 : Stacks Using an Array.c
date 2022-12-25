5beae68d03ade735e53f0629c72c50f28a5bc753
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-12-25 15:13:05 +0530

    Update M 101 : Stacks Using an Array.c

--- M 101 : Stacks Using an Array.c
+++ M 101 : Stacks Using an Array.c
@@ -10,19 +10,18 @@ void push(int x){
     stackArr[top]=x;
 } //- Insert an element onto the top of the stack
 int peek(){
-return stackArr[top];
+    return stackArr[top];
 } //- Returns the topmost element on the stack
 void pop(){
-stackArr[top]=NULL;
-top--;
+    stackArr[top]=NULL;
+    top--;
 } //- Removes an element from the top of the stack
 int empty(){
-if(top==-1)
-return 1;
-else
-return 0;
+    if(top==-1)
+        return 1;
+    else
+        return 0;
 }// - Returns 1 if the stack is empty and 0 otherwise
-
 //TAIL
 int main()
 {
